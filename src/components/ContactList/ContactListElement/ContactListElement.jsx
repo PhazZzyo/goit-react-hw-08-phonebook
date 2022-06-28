49324d7c83c1d8e0f7a8b24d0e816f8394f5492f
Alex <41620816+PhazZzyo@users.noreply.github.com>
2022-06-28 15:00:45 +0300

    Update ContactListElement.jsx

--- src/components/ContactList/ContactListElement/ContactListElement.jsx
+++ src/components/ContactList/ContactListElement/ContactListElement.jsx
@@ -1,12 +1,11 @@
-// import { useDispatch } from 'react-redux';
-// import { deleteContact } from 'redux/contacts/contactsActions';
 import { useDeleteContactMutation } from 'services/contactsAPI';
-import Loader from 'components/Loader/Loader';
+// import Loader from 'components/Loader/Loader';
 import PropTypes from 'prop-types';
 import style from './ContactListElement.module.css';
 
 const ContactListElement = ({ id, name, number }) => {
-  const [deleteContact, { isLoading }] = useDeleteContactMutation();
+  // const [deleteContact, { isLoading }] = useDeleteContactMutation();
+  const [deleteContact] = useDeleteContactMutation();
   return (
     <>
       {/* {isLoading} && <Loader color={'#3f51b5'} size={32} /> */}
